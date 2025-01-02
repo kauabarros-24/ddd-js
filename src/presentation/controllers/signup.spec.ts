8a7adba078a4da564fa254c71516c8a9872d739e
kaubarros-24 <martinsbarroskaua@gmail.com>
2025-01-02 14:04:22 -0300

    chore: add email verification controller

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -12,5 +12,19 @@ describe('SignupController', () => {
         }
         const http_response = sut.handle(http_request)
         expect(http_response.statusCode).toBe(400)
+        expect(http_response.body).toEqual(new Error('Missing param: name'))
+    })
+    test ('Should return 400 if no email is provided', () => {
+        const sut = new SignupController()
+        const http_request = {
+            body: {
+                name: "any_name",
+                password: "any_password",
+                passwordConfirm: "any_password"
+            }
+        }
+        const http_response = sut.handle(http_request)
+        expect(http_response.statusCode).toBe(400)
+        expect(http_response.body).toEqual(new Error("Missing param: email"))
     })
 })
--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -2,8 +2,17 @@ import { REFUSED } from "dns";
 
 export class SignupController {
     handle(http_response: any): any {
-        return {
-            statusCode: 400
+        if (!http_response.body.name) {
+            return {
+                statusCode: 400,
+                body: new Error('Missing param: name')
+            }
+        }
+        else if(!http_response.body.email) {
+            return {
+                statusCode: 400,
+                body: new Error('Missing param: email')
+            }
         }
     }
 }
